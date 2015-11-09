66d933ed1f22a3c35f62d33ce5e3fa54b3765bf0
Javier Gonzalez <javichu.12@gmail.com>
2015-11-09 10:53:50 +0000

    -Connection to url

--- app/src/main/java/com/example/javichu/myapplication/MainActivityFragment.java
+++ app/src/main/java/com/example/javichu/myapplication/MainActivityFragment.java
@@ -2,15 +2,23 @@ package com.example.javichu.myapplication;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -25,6 +33,47 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String forecastJsonStr = null;
+
+        try{
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7&APPID=25ad9928d336752b2ac8742b78579796");
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null){
+                return  null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+
+            while ((line = reader.readLine()) != null){
+                buffer.append(line + "\n");
+            }
+
+            if(buffer.length() == 0) return null;
+
+            forecastJsonStr = buffer.toString();
+
+        }catch (IOException e){
+            Log.e("PlaceholderFragment", "Error", e);
+            return null;
+        } finally {
+            if (urlConnection != null) urlConnection.disconnect();
+            if(reader != null){
+                try {
+                    reader.close();
+                }catch (final IOException e){
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
+
         String[] forecastArray = {
                 "Today - Sunny - 88/63",
                 "Tomorrow - Foggy - 70/40",
